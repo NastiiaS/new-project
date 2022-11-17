ee40b7a0af0ef09bc8d3e57bc2a98a20f8683409
NastiiaS <nastia.space111@gmail.com>
2022-11-17 14:22:47 +0200

    Added weather project (html,css,js)

---
+++ src/index.js
@@ -0,0 +1,48 @@
+let now = new Date();
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "Jun",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let currentDayMonth = document.querySelector("#dateMon");
+let currentTime = document.querySelector("#time");
+currentDayMonth.innerHTML = ` ${date} ${month}`;
+currentTime.innerHTML = `${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "fedaca8118b0cf269ccc3d2228739a0c";
+  let city = document.querySelector("#search-city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
